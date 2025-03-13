dfc8193d197aba1f1e23db9dc6c459680f75d0c9
assnssters <dinhductri18@gmail.com>
2025-03-13 12:16:22 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -6,6 +6,9 @@ yellow='\033[1;33m'
 blue='\033[1;34m'
 light_cyan='\033[1;96m'
 reset='\033[0m'
+lines() {
+echo
+}
 cls() {
     clear
 }
@@ -47,12 +50,15 @@ fi
 
 # Update Kho Lưu Trữ và Tải QEMU-KVM, Build SWTPM
 echo -ne "$yellow Đang Cập nhật kho lưu trữ..."
+lines
 apt update -y > /dev/null 2>&1
 spin $!
 echo -ne "$yellow Ðang tải và cài đặt QEMU-KVM..."
+lines
 apt install p7zip-full qemu-kvm qemu-utils -y > /dev/null 2>&1
 spin $!
 echo -ne "$yellow BUILD SWTPM..(mất nhiều thời gian)$reset"
+lines
 echo Đang tải...
 sleep 3
 apt install git g++ gcc automake autoconf libtool make gcc libc-dev libssl-dev pkg-config libtasn1-6-dev libjson-glib-dev expect gawk socat libseccomp-dev -y > /dev/null 2>&1
@@ -77,14 +83,12 @@ echo -ne """"$reset"hãy chọn Hệ Điều hành bạn muốn tải:
     1. Windows 11 x64 English Interational
     2. Windows Server 2025 x64 English
     3. Windows Server 2012 R2 x64 English
-    4. Custom ISO ( LINK )
 """
 read -p "chọn ( Số ): " stos
     case $stos in
         1) export ados="$(curl "https://raw.githubusercontent.com/assnssters/GhCodeSpaceVPS/refs/heads/main/11link.txt")";break;;
         2) export ados="https://go.microsoft.com/fwlink/?linkid=2293312&clcid=0x409&culture=en-us&country=us";break;;
         3) export ados="https://go.microsoft.com/fwlink/p/?LinkID=2195443&clcid=0x409&culture=en-us&country=US";break;;
-        4) export ados=$(read -p "link: " ah && echo $ah );break;;
         *) echo -ne "$red Unknown Hãy chạy lại.$reset";;
     esac
 clear
@@ -123,7 +127,7 @@ cls
 done
 qemu-img create -f qcow2 /mnt/os.qcow2 401G
 cls
-echo "kết nối qua VNC ports(5900)
+echo "kết nối qua VNC ports(5900)"
 sudo cpulimit -l 80 -- sudo kvm \
     -cpu host,+topoext,hv_relaxed,hv_spinlocks=0x1fff,hv-passthrough,+pae,+nx,kvm=on,+svm \
     -smp $cpu \
@@ -139,7 +143,7 @@ sudo cpulimit -l 80 -- sudo kvm \
     -device virtio-rng-pci \
     -enable-kvm \
     -device nvme,serial=deadbeef,drive=nvm \
-    -drive file=/mnt/os.qcow2,ìf=none,id=nvm
+    -drive file=/mnt/os.qcow2,ìf=none,id=nvm \
     -drive file=/mnt/os.iso,media=cdrom \
     -drive if=pflash,format=raw,readonly=off,file=/usr/share/ovmf/OVMF.fd \
     -uuid e47ddb84-fb4d-46f9-b531-14bb15156336 \
